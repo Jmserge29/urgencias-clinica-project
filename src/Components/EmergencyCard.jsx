de23004098d78481455e9b3905be699a886cad22
Jmserge29 <Jmserge29@gmail.com>
2023-10-11 08:42:02 -0500

    Idk5

--- src/Components/EmergencyCard.jsx
+++ src/Components/EmergencyCard.jsx
@@ -2,11 +2,10 @@ import axios from 'axios'
 import React, { useState } from 'react'
 
 function EmergencyCard({emergency}) {
-    console.log("emergencia :" , emergency)
     const [urgency, setUrgency] = useState([])
     const loadEmergency = async() => {
       await axios.get(`https://urgencias-servidor-project.vercel.app/Emergency/getEmergencyById/${emergency}`).then((res) => {
-        console.log(res.data)
+        setUrgency(res.data)
       })
     }
     loadEmergency()
