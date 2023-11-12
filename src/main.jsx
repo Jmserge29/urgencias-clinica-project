213b0ef40766aad86903d4d8f5bcbe126eb1c808
Jmserge29 <93602488+Jmserge29@users.noreply.github.com>
2023-11-12 17:48:28 -0500

    continue

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Landing from "./Pages/Home.jsx";
 import "./index.css";
 import Portal from "./Pages/Portal.jsx";
-import Assistance from "./Pages/Assistance.jsx";
 import Register from "./Pages/Register.jsx";
 import SignInAdministrative from "./Pages/SignInAdministrative.jsx"
 import SignInLaboral from "./Pages/SignInLaboral.jsx";
@@ -20,17 +19,20 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
       <Route exact path="/" element={<Landing />} />
       <Route path="/home" element={<Landing />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/plataform" element={<Plataforma />} />
+
+
+
+      
       <Route path="/portal" element={<Portal />} />
       <Route path="/me" element={<PatientPortal />} />
       <Route path="/auth/administrative" element={<SignInAdministrative />} />
       <Route path="/auth/doctor" element={<SignInLaboral />} />
       <Route path="/auth/patient" element={<SignInPatient />} />
-      <Route path="/assistance" element={<Assistance />} />
       <Route path="/assistance/patients" element={<ListPatients />} />
       <Route path="/register" element={<Register />} />
       <Route path="/test" element={<Test />} />
-      <Route path="/plataform" element={<Plataforma />} />
-      <Route path="/login" element={<Login />} />
 
 
 
