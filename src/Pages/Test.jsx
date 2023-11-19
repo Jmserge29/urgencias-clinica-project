6b0f87976c7d7671c9f4f86ff8ad9c7ce29fe32c
Jmserge29 <93602488+Jmserge29@users.noreply.github.com>
2023-11-19 16:47:18 -0500

    Begining aplicate estructure of the data

--- src/Pages/Test.jsx
+++ src/Pages/Test.jsx
@@ -4,13 +4,22 @@ import React, { useState, useEffect } from "react";
 import Isotope from "isotope-layout";
 import axios from "axios";
 import { QueueListIcon } from "@heroicons/react/24/solid";
+import ModalEstructure from "../Components/Modals/Plataform/ModalEstructure";
 
 export default function Test() {
   const [isotope, setIsotope] = useState(null);
   const [option, setOption] = useState(".emergency");
   const [emergenciasSeleccionadas, setEmergenciasSeleccionadas] = useState([]);
-  const [data, setData] = useState([]); // Supongamos que recibes la data como un estado
+  const [data, setData] = useState([]); 
+  let [isOpen, setIsOpen] = useState(false);
 
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
   const getEmergenciesData = async () => {
     try {
       await axios
@@ -33,89 +42,6 @@ export default function Test() {
     getEmergenciesData();
     filterRecords(".emergency");
 
-    // const emergencyData = [
-    //   {
-    //     _id: "1",
-    //     doctorAsignado: null,
-    //     paciente: "paciente_1",
-    //     medicamentosRecetados: [],
-    //     motivos_consulta: "Gripa",
-    //     tratamiento: "",
-    //     clasificacion: "Sin atender",
-    //     hora: "Mon Nov 13 2023 23:30:08 GMT-0500 (hora estándar de Colombia)",
-    //     createdAt: "2023-11-14T04:30:08.932Z",
-    //     updatedAt: "2023-11-14T04:30:08.932Z",
-    //     __v: 0,
-    //   },
-    //   {
-    //     _id: "2",
-    //     doctorAsignado: null,
-    //     paciente: "paciente_2",
-    //     medicamentosRecetados: [],
-    //     motivos_consulta: "Dolor de espalda",
-    //     tratamiento: "",
-    //     clasificacion: "Admitido a Urgencias",
-    //     hora: "Mon Nov 13 2023 23:45:08 GMT-0500 (hora estándar de Colombia)",
-    //     createdAt: "2023-11-14T04:45:08.932Z",
-    //     updatedAt: "2023-11-14T04:45:08.932Z",
-    //     __v: 0,
-    //   },
-    //   {
-    //     _id: "3",
-    //     doctorAsignado: null,
-    //     paciente: "paciente_3",
-    //     medicamentosRecetados: [],
-    //     motivos_consulta: "Amildalitis",
-    //     tratamiento: "",
-    //     clasificacion: "Dado de Alta con Cita Prioritaria",
-    //     hora: "Mon Nov 13 2023 23:45:08 GMT-0500 (hora estándar de Colombia)",
-    //     createdAt: "2023-11-14T04:45:08.932Z",
-    //     updatedAt: "2023-11-14T04:45:08.932Z",
-    //     __v: 0,
-    //   },
-    //   {
-    //     _id: "4",
-    //     doctorAsignado: null,
-    //     paciente: "paciente_4",
-    //     medicamentosRecetados: [],
-    //     motivos_consulta:
-    //       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. A nobis corrupti, totam sint qui error asperiores beatae perferendis veritatis. Dolorum optio ex, doloremque suscipit cum quas numquam aut facilis possimus.",
-    //     tratamiento: "",
-    //     clasificacion: "Admitido a Urgencias",
-    //     hora: "Mon Nov 13 2023 23:45:08 GMT-0500 (hora estándar de Colombia)",
-    //     createdAt: "2023-11-14T04:45:08.932Z",
-    //     updatedAt: "2023-11-14T04:45:08.932Z",
-    //     __v: 0,
-    //   },
-    //   {
-    //     _id: "5",
-    //     doctorAsignado: null,
-    //     paciente: "paciente_5",
-    //     medicamentosRecetados: [],
-    //     motivos_consulta: "Carillas",
-    //     tratamiento: "",
-    //     clasificacion: "Dado de Alta con Cita Prioritaria",
-    //     hora: "Mon Nov 13 2023 23:45:08 GMT-0500 (hora estándar de Colombia)",
-    //     createdAt: "2023-11-14T04:45:08.932Z",
-    //     updatedAt: "2023-11-14T04:45:08.932Z",
-    //     __v: 0,
-    //   },
-    //   {
-    //     _id: "6",
-    //     doctorAsignado: null,
-    //     paciente: "paciente_6",
-    //     medicamentosRecetados: [],
-    //     motivos_consulta: "Inflamacion de costillas",
-    //     tratamiento: "",
-    //     clasificacion: "Dado de Alta con Tratamiento",
-    //     hora: "Mon Nov 13 2023 23:45:08 GMT-0500 (hora estándar de Colombia)",
-    //     createdAt: "2023-11-14T04:45:08.932Z",
-    //     updatedAt: "2023-11-14T04:45:08.932Z",
-    //     __v: 0,
-    //   },
-
-    //   // ... otros objetos de emergencia
-    // ];
   }, []);
 
   // Filtrar la data en diferentes arrays según la clasificación
@@ -209,14 +135,9 @@ export default function Test() {
             );
           })}
         </div>
-        {/* <div className="mx-32 text-center ">
-          <PatientCard />
-        </div> */}
-
         <div className="medical-records text-center mx-32">
-          {/* Aquí puedes mapear tus registros médicos */}
+          {/* Aquí mapea los registros médicos */}
           <div className="record emergency">
-            Emergencia sin atender ({sinAtender.length})
             {sinAtender.map((data, i) => {
               return (
                 <div key={i}>
@@ -230,7 +151,6 @@ export default function Test() {
             })}
           </div>
           <div className="record admitted">
-            Admitido a Urgencias ({admitidoUrgencias.length})
             {data ? (
               <>
                 {admitidoUrgencias.map((data, i) => {
@@ -250,13 +170,11 @@ export default function Test() {
             )}
           </div>
           <div className="record discharged-treatment">
-            Dado de Alta con Tratamiento ({altaConTratamiento.length})
             {altaConTratamiento.map((data, i) => {
               return <div key={i}>{data._id}</div>;
             })}
           </div>
           <div className="record discharged-appointment">
-            Dado de Alta con Cita Prioritaria ({altaConCitaPrioritaria.length})
             {altaConCitaPrioritaria.map((data, i) => {
               return (
                 <div key={i}>
@@ -271,9 +189,10 @@ export default function Test() {
           </div>
         </div>
       </div>
-      <div className="fixed bottom-8 hover:cursor-pointer right-12 shadow-lg text-rose-600 h-14 w-14 rounded-full ">
-        <QueueListIcon className="h-14" />
+      <div onClick={() => openModal()} className="fixed bottom-8 hover:cursor-pointer right-12 shadow-lg text-rose-600 flex justify-center items-center h-20 w-20 rounded-full hover:scale-110 transition delay-150 duration-300 ease-in-out  ">
+        <QueueListIcon className="h-20 p-3" />
       </div>
+      <ModalEstructure closeModal={closeModal} isOpen={isOpen} />
     </>
   );
 }
