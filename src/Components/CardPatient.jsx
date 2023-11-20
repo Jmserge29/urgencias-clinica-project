11026298b39c4103cd473389371141c0a095e257
Jmserge29 <93602488+Jmserge29@users.noreply.github.com>
2023-11-20 01:45:33 -0500

    Testing 1, completed :)

--- src/Components/CardPatient.jsx
+++ src/Components/CardPatient.jsx
@@ -1,9 +1,12 @@
 import axios from 'axios'
 import { useState } from 'react'
 import ElementCardUser from './ElementCardUser'
+import ModalDiagnostic from './Modals/Plataform/ModalDiagnostic'
 
 function CardPatient({urgencia}) {
   const [urgency, setUrgency] = useState([])
+  let [isOpen, setIsOpen] = useState(true)
+
   const loadEmergency = async() => {
     await axios.get(`https://urgencias-servidor-project.vercel.app/Emergency/getEmergencyById/${urgencia}`).then((res) => {
       setUrgency(res.data)
@@ -12,7 +15,7 @@ function CardPatient({urgencia}) {
   loadEmergency()
   return (
     <>
-          <article className="rounded-xl mt-8 bg-white p-3 h-80 mx-4 shadow-lg hover:shadow-xl hover:transform hover:scale-105 duration-300 ">
+    <article className="rounded-xl mt-8 bg-white p-3 h-80 mx-4 shadow-lg hover:shadow-xl hover:transform hover:scale-105 duration-300 ">
       <a href="#">
         <div className="relative flex items-end overflow-hidden rounded-xl">
           <img className=' h-64 w-full bg-cover' src="https://www.softzone.es/app/uploads/2018/04/guest.png?x=480&quality=40" alt="Background User Photo" />
@@ -25,6 +28,7 @@ function CardPatient({urgencia}) {
 
       </a>
     </article>
+    <ModalDiagnostic isOpen={isOpen} setIsOpen={setIsOpen}/>
     </>
   )
 }
