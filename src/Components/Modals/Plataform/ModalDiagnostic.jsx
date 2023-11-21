65b083fd9076bf937aea616bbea10013d2c56e7c
Jmserge29 <93602488+Jmserge29@users.noreply.github.com>
2023-11-21 17:27:33 -0500

    d

--- src/Components/Modals/Plataform/ModalDiagnostic.jsx
+++ src/Components/Modals/Plataform/ModalDiagnostic.jsx
@@ -213,7 +213,7 @@ function ModalDiagnostic({ isOpen, closeModal }) {
                           <label className="text-base mt-6 font-medium">
                             Medicamentos
                           </label>
-                          <select id="underline_select" class="block py-2.5 px-0 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer">
+                          <select id="underline_select" class="block py-2.5 mb-12 px-2 w-full text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none  focus:outline-none focus:ring-0 focus:border-gray-200 peer">
                             <option selected></option>
                             {apiMedicines.map((data) => {
                               return(
