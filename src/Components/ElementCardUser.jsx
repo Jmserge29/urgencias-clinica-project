27cd893dc70bf5bbe28aaf1cc1a96bf6f9ac7c6f
Jmserge29 <93602488+Jmserge29@users.noreply.github.com>
2023-11-21 22:23:28 -0500

    Error database 2.7.2.6

--- src/Components/ElementCardUser.jsx
+++ src/Components/ElementCardUser.jsx
@@ -1,26 +1,36 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-function ElementCardUser({paciente}) {
-  if(paciente) console.log(paciente)
-    // const[user, setUser] = useState([])
-    // const loadDataUser = async() => {
-    //     await axios.get(`https://urgencias-servidor-project.vercel.app/User/getUserById/${paciente}`).then((res) => {
-    //       setUser(res.data.usuario)
-    //       // setuserInformation(res.data.usuario)
-    //     }).catch((err) => {
-    //       console.log("ELEMENT ERRR")
-    //       console.log(err)
-    //     })
-    // }
-    // useEffect(async() => {
-    //   await loadDataUser()
-    // }, [])
-  return (<>
-    <div className="flex my-3 justify-center items-center text-lg font-semibold font-mono">
-      {/* {user.nombre} {user.apellido} */}user
-    </div>
-    <span className="text-center flex justify-center rounded-2xl bg-slate-900 text-white py-3 px-4">Edad: 85</span></>
+function ElementCardUser({ paciente }) {
+  const [user, setUser] = useState([]);
+  const loadDataUser = async () => {
+    if (paciente) {
+      await axios
+        .get(
+          `https://urgencias-servidor-project.vercel.app/User/getUserById/${paciente}`
+        )
+        .then((res) => {
+          setUser(res.data.usuario);
+          // setuserInformation(res.data.usuario)
+        })
+        .catch((err) => {
+          console.log("ELEMENT ERRR");
+          console.log(err);
+        });
+    }
+  };
+  useEffect(async () => {
+    await loadDataUser();
+  }, []);
+  return (
+    <>
+      <div className="flex my-3 justify-center items-center text-lg font-semibold font-mono">
+        {/* {user.nombre} {user.apellido} */}user
+      </div>
+      <span className="text-center flex justify-center rounded-2xl bg-slate-900 text-white py-3 px-4">
+        Edad: 85
+      </span>
+    </>
   );
 }
 
