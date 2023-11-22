b051a0a2197337397f5bf9a34da1094d66a8ae8f
Jmserge29 <93602488+Jmserge29@users.noreply.github.com>
2023-11-21 21:37:21 -0500

    Error database 2.5

--- src/Pages/Portal.jsx
+++ src/Pages/Portal.jsx
@@ -10,26 +10,26 @@ function Portal() {
   const usuarioJSON = localStorage.getItem('doctor');
   const doctor = JSON.parse(usuarioJSON);
 
-  // const getIfnormationUser = async() => {
-  //   try {
-  //     await axios
-  //     .get(
-  //       `https://urgencias-servidor-project.vercel.app/User/getUserById/${doctor._id}`
-  //     )
-  //     .then((res) => {
-  //       const usuarioJSON = JSON.stringify(res.data.usuario);
-  //         localStorage.setItem('doctor', usuarioJSON);
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  //   } catch (error) {
-  //     console.log("error error")
-  //   }
-  // }
-  // useEffect(async() => {
-  //   getIfnormationUser()
-  // }, [])
+  const getIfnormationUser = async() => {
+    try {
+      await axios
+      .get(
+        `https://urgencias-servidor-project.vercel.app/User/getUserById/${doctor._id}`
+      )
+      .then((res) => {
+        const usuarioJSON = JSON.stringify(res.data.usuario);
+          localStorage.setItem('doctor', usuarioJSON);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    } catch (error) {
+      console.log("error error")
+    }
+  }
+  useEffect(async() => {
+    getIfnormationUser()
+  }, [])
   return (<>
     <SideBar/>
     <div className='sm:ml-44 mt-16 sm:mr-12'>
